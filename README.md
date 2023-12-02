63c9f9dd74dd97d93455aba955d4a54ef69de4f0
bogil SO <sotomas232@gmail.com>
2023-12-02 22:09:49 +0900

    final

--- README.md
+++ README.md
@@ -4,11 +4,11 @@
 
 ## 목차
 
-- [Motive](#introduction)
+- [Motivation](#introduction)
 - [Description](#installation)
 - [Usage](#usage)
 
-## Motive <a name="introduction"></a>
+## Motivation <a name="introduction"></a>
 
 * 많은 사람들이 vscode를 사용을 하는데 하다보면 장시간 스트레칭도 안하고 작업을 진행해 스트레칭을 권장할 겸 만들게 되었다.
 
@@ -20,17 +20,27 @@
 ![시작알림](./image/start.png)
 
 2. 한시간이 지나면 해당 알림이 온다.  
-![시작알림](./image/result.png)
+![스트레칭](./image/result.png)
 
-3. 예를 눌렀을 시  
-![시작알림](./image/result_yes.png) 
+3. 예를 눌렀을 시 (체조 동영상 url로 이동할 수 있다)   
+![예](./image/result_yes.png) 
 
 4. 아니오를 눌렀을 시  
-![시작알림](./image/result_no.png)
+![아니오](./image/result_no.png)
 
-5. 밤 11시에 알려주는 현재 시각  
-![시작알림](./image/pm11.png)
+5. 밤 11시에 나오는 알림  
+![밤 11시](./image/pm11.png)
 ## Usage <a name="usage"></a>
 
-프로젝트를 사용하는 방법에 대한 내용을 작성합니다.
+1. 다운받는 방법
+    * vscode extensions에 stretch-alarm이라고 검색하고 install한 후 command palette에서 `stretching`을 입력하면 사용해볼 수 있다.   
+    ![다운](./image/alarm.png)
+[Marketplace](https://marketplace.visualstudio.com/items?itemName=sobogil.stretch-alarm&ssr=false#review-details)
+
+2. 시간 지정하고 만들어 보기
+    ```
+    git clone https://github.com/sobogil/stretch-alarm.git
+    ```
+    src/extension.ts파일에서 시간 값을 변경하여 본인에게 원하는 알람으로 만들면 된다.   
+
 **Enjoy!**
--- src/extension.ts
+++ src/extension.ts
@@ -1,41 +1,50 @@
 import * as vscode from 'vscode';
 
+let intervalId: NodeJS.Timeout | null = null;
+let messages: string[] = [
+    '⏰스트레칭 시간입니다. 일어서서 간단히 스트레칭을 하세요.',
+    '💪스트레칭 시간! 건강을 위해 잠깐 일어나세요.',
+    '🏋️‍♀️잠깐 스트레칭을 하고 오세요. 몸과 마음이 가벼워질 거예요.'
+];
+
 export function activate(context: vscode.ExtensionContext) {
     console.log('Congratulations, your extension is now active!');
-
-    vscode.window.showInformationMessage('⚠️스트레칭 알람이 실행 됐습니다. 한시간마다 알람이 울릴 것입니다.');
+    vscode.window.showInformationMessage('⚠️스트레칭 알람이 실행 됐습니다. 설정된 시간마다 알람이 울릴 것입니다.');
 
     let disposable = vscode.commands.registerCommand('extension.stretch', () => {
-        setInterval(() => {
-            const currentDate = new Date();
-            const currentHour = currentDate.getHours();
-            const currentMinute = currentDate.getMinutes();
-            if (currentHour === 23 && currentMinute === 0) {
-                vscode.window.showInformationMessage('❗하루가 지나기 1시간 전입니다.❗');
-            }
-        }, 10000); // 1분마다 현재 시간을 체크
+        if (intervalId) {
+            clearInterval(intervalId);
+            vscode.window.showInformationMessage('⏰스트레칭 알람이 종료되었습니다.');
+        } else {
+            const stretchingAlert = () => {
+                const message = messages[Math.floor(Math.random() * messages.length)];
+                vscode.window.showInformationMessage(message, '예', '아니오')
+                    .then(selection => {
+                        if (selection === '예') {
+                            vscode.window.showInformationMessage('💪현명한 선택입니다. 당신은 방금 거북목, 골반 비대칭, 일자목 등 안좋은 척추 질환을 예방하였으며 앞으로 오래오래 잘 살 수 있을 것입니다.');
+                            vscode.env.openExternal(vscode.Uri.parse('https://www.youtube.com/watch?v=WsW5eFmdEuk')); //스트레칭 영상 링크
+                        } else {
+                            vscode.window.showInformationMessage('😡😡😡스트레칭을 하지 않으신다고요? 오랜 시간 한자세를 유지하면서 당신의 근육 및 관절 건강은 상태가 매우 안좋은 상태입니다. 만약 당신이 이글을 읽고도 일어서지 않을 시 근육 경직 및 불편함, 관절통증, 잘못된 자세로 인한 퇴행성 변화, 스트레스 증가, 운동 성능 저하가 될 것이며 당신은 나중에 평생 후회하면서 살아 갈 것입니다.')
+                        }
+                    });
+            };
 
-        const stretchingAlert = () => {
-            vscode.window.showInformationMessage('⏰스트레칭 시간입니다. 일어서서 간단히 스트레칭을 하세요.', '예', '아니오')
-                .then(selection => {
-                    if (selection === '예') {
-                        vscode.window.showInformationMessage('💪현명한 선택입니다. 당신은 방금 거북목, 골반 비대칭, 일자목 등 안좋은 척추 질환을 예방하였으며 앞으로 오래오래 잘 살 수 있을 것입니다.');
-                        vscode.env.openExternal(vscode.Uri.parse('https://www.youtube.com/watch?v=WsW5eFmdEuk')); //스트레칭 영상 링크(현재 국군도수 체조)
-                    } else {
-                        vscode.window.showInformationMessage('😡😡😡스트레칭을 하지 않으신다고요? 오랜 시간 한자세를 유지하면서 당신의 근육 및 관절 건강은 상태가 매우 안좋은 상태입니다. 만약 당신이 이글을 읽고도 일어서지 않을 시 근육 경직 및 불편함, 관절통증, 잘못된 자세로 인한 퇴행성 변화, 스트레스 증가, 운동 성능 저하가 될 것이며 당신은 나중에 평생 후회하면서 살아 갈 것입니다.')
-                    }
-                });
-        };
+            // 즉시 알람 실행
+            stretchingAlert();
 
-        // 즉시 알람 실행
-        stretchingAlert();
-
-        // 설정한 시간 간격으로 알람 반복 실행
-        setInterval(stretchingAlert, 3600000); // 60분마다 알림
-
+            // 설정한 시간 간격으로 알람 반복 실행
+            const interval = vscode.workspace.getConfiguration().get<number>('stretch.interval', 60);
+            intervalId = setInterval(stretchingAlert, interval * 60000);  // 사용자가 설정한 시간마다 알람
+            vscode.window.showInformationMessage(`⏰스트레칭 알람이 ${interval}분 간격으로 설정되었습니다.`);
+        }
     });
 
     context.subscriptions.push(disposable);
 }
 
-export function deactivate() {}
+export function deactivate() {
+    if (intervalId) {
+        clearInterval(intervalId);
+        vscode.window.showInformationMessage('⏰스트레칭 알람이 종료되었습니다.');
+    }
+}
